0fbc1ae38d6ad6e3d8bdcd30ca2c009e82e355a5
Antonio González Viegas <antoniogviegas@hotmail.com>
2025-03-13 13:54:16 +0100

    add civil example

--- main.ts
+++ main.ts
@@ -9,6 +9,7 @@ import { Extrude } from "./geometries/extrude";
 import { CurveParabola } from "./geometries/parabola";
 import { CurveClothoid } from './geometries/clothoid';
 import { CurveArc } from "./geometries/arc";
+import { CivilReader } from "./civil-reader";
 
 async function main() {
   // Set up scene
@@ -83,15 +84,42 @@ async function main() {
   // gui.add(clothoid, "SegmentLength", -10, 10, 0.05).onChange(() => clothoid.update(api));
 
   // Arc
-  const arc = new CurveArc(api);
-  world.scene.three.add(arc.mesh);
-  gui.add(arc, "numSegments", 3, 100, 1).onChange(() => arc.update(api));
-  gui.add(arc, "radiusX", 1, 10, 0.05).onChange(() => arc.update(api));
-  gui.add(arc, "radiusY", 1, 10, 0.05).onChange(() => arc.update(api));
-  gui.add(arc, "startRad", 0, 6.28, 0.05).onChange(() => arc.update(api));
-  gui.add(arc, "endRad", 0, 6.28, 0.05).onChange(() => arc.update(api));
-  gui.add(arc, "swap").onChange(() => arc.update(api));
-  gui.add(arc, "normalToCenterEnding").onChange(() => arc.update(api));
+  // const arc = new CurveArc(api);
+  // world.scene.three.add(arc.mesh);
+  // gui.add(arc, "numSegments", 3, 100, 1).onChange(() => arc.update(api));
+  // gui.add(arc, "radiusX", 1, 10, 0.05).onChange(() => arc.update(api));
+  // gui.add(arc, "radiusY", 1, 10, 0.05).onChange(() => arc.update(api));
+  // gui.add(arc, "startRad", 0, 6.28, 0.05).onChange(() => arc.update(api));
+  // gui.add(arc, "endRad", 0, 6.28, 0.05).onChange(() => arc.update(api));
+  // gui.add(arc, "swap").onChange(() => arc.update(api));
+  // gui.add(arc, "normalToCenterEnding").onChange(() => arc.update(api));
+
+  // Open model
+
+  const fetched = await fetch("(E28)_CARRETERA_10.94_4X3.ifc");
+  const arrayBuffer = await fetched.arrayBuffer();
+  const uint8Array = new Uint8Array(arrayBuffer);
+  const modelId = api.OpenModel(uint8Array, {
+    COORDINATE_TO_ORIGIN: true,
+  });
+
+  api.StreamAllMeshes(modelId, () => {});
+
+  // Get explicit aligments
+
+  const civilReader = new CivilReader();
+  const alignments = civilReader.read(api);
+
+  console.log(alignments);
+  // console.log(ifcCrossSection2D);
+  // console.log(ifcCrossSection3D);
+
+  for(const [,alignment] of alignments.alignments) {
+    for(const {mesh} of alignment.absolute) {
+      world.scene.three.add(mesh);
+    }
+  }
+
 }
 
 main();
